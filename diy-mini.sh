e632cba3884b1bea3cb0b0f614d4ccfe65a4d955
cr3hf <carine3hf@gmail.com>
2024-09-20 09:09:25 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -34,6 +34,7 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 
 # 科学上网插件
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 git clone --depth=1 https://github.com/liuran001/openwrt-packages/tree/packages/luci-app-ssr-plus package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
