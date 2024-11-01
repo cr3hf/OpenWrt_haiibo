22a848e7ce8414f6ac154dd1bc7a9ed123354c89
cr3hf <carine3hf@gmail.com>
2024-11-01 16:38:07 +0800

    Update test-script.sh

--- test-script.sh
+++ test-script.sh
@@ -69,6 +69,8 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 # git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
+git_sparse_clone main https://github.com/jjm2473/openwrt-apps homebox luci-app-fan luci-app-homebox
+
 
 # 科学上网插件
 #git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
